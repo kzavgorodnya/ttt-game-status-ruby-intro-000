5fee3f124a0ead9148672a3656639ad97d3371b6
kzavgorodnya <kateryna.zavgorodnya@gmail.com>
2018-07-30 19:39:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,10 +5,16 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8], # las row
-  [0,4,8],
-  [2,4,6]
+  [0,1,2])
+  expect(WIN_COMBINATIONS).to include_array([3,4,5])
+  expect(WIN_COMBINATIONS).to include_array([6,7,8])
+  expect(WIN_COMBINATIONS).to include_array([0,3,6])
+  expect(WIN_COMBINATIONS).to include_array([1,4,7])
+  expect(WIN_COMBINATIONS).to include_array([2,5,8])
+  expect(WIN_COMBINATIONS).to include_array([0,4,8])
+  expect(WIN_COMBINATIONS).to include_array([6,4,2]
+
+
+
 
 ]
