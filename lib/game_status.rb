63b6620dc06b39d017f69b138df089359adc91e2
kzavgorodnya <kateryna.zavgorodnya@gmail.com>
2018-07-30 20:06:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,6 @@ def won? (board)
 
   if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
     "X won in the top row"
-if boardWIN_COMBINATIONS[0]
+if board[WIN_COMBINATIONS[0]] == "X" &&  board[WIN_COMBINATIONS[]]
 
 end
